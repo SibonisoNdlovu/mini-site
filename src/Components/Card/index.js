769616fb0037d31b59d2e312a959adb868bc5f91
Siboniso Ndlovu <sibonisow.ndlovu@gmail.com>
2022-06-13 00:44:57 +0200

    small changes

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -5,10 +5,11 @@ export const Card = ({ data }) => {
     <section>
       <body className="card">
         <h1>Full Name: {data.first_name + " " + data.last_name }</h1>
-        <p className="title">Address: {data.address}</p>
-        <p className="title">City: {data.city}</p>        
-        <p className="title">Country: {data.country}</p>        
-        <p className="title">Zip Code: {data.zip_code}</p>
+        <p>DOB: {data.date_of_birth}</p>
+        <p>Address: {data.address}</p>
+        <p>City: {data.city}</p>        
+        <p>Country: {data.country}</p>        
+        <p>Zip Code: {data.zip_code}</p>
         <p>Email:  {data.email_address}</p>
         </body>
     </section>
